1621b7efe370ffd7076b8537912a08d3640eae20
Hugo Hörnquist <hugo@lysator.liu.se>
2019-12-09 14:15:24 +0100

    Fix memmory problems.

--- 2019/7/part-1.c
+++ 2019/7/part-1.c
@@ -6,152 +6,9 @@
 
 #include "instr.h"
 #include "fifo.h"
+#include "process.h"
 
 #define debug 0
-
-#define PAGES (0x100 - 1)
-
-typedef enum process_states {
-	none = 0,
-	ready, running, waiting, done,
-	error,
-} process_state;
-
-char* state_name[] = {
-	[none]    = "none",
-	[ready]   = "ready",
-	[running] = "running",
-	[waiting] = "waiting",
-	[done]    = "done",
-	[error]   = "error",
-};
-
-typedef struct proc {
-	int pid;
-	size_t pc;
-	size_t size;
-	process_state state;
-	fifo *input, *output;
-} proc;
-
-
-/* primary memory */
-struct page { 
-	proc proc;
-	int mem[0x1000];
-} mem[PAGES];
-
-struct process_list_link {
-	struct proc* proc;
-	struct process_list_link* next;
-} head, *tail, links[0x100 - 1];
-
-/* Implicit overflow */
-uint8_t current_link_slot = 0;
-
-int current_pid = 0;
-int current_page = 0;
-
-int* proc_mem(proc* p) {
-	return ((int*) (((uint8_t*) p) + sizeof(struct proc)));
-}
-
-void print_process (proc* p) {
-	printf("process = {\n\tpid = %i\n\tpc = %li\n\tstate = %s\n\tmem = [%li]\n\t\t",
-			p->pid, p->pc, state_name[p->state], p->size);
-	for (size_t i = 0; i < p->size; i++) {
-		if (i == p->pc) {
-			printf("[%i], ", proc_mem(p)[i]);
-		} else {
-			printf("%i, ", proc_mem(p)[i]);
-		}
-	}
-	printf("\n\t}\n}\n");
-}
-
-
-struct proc* find_page () {
-	struct proc* proc = NULL;
-
-	for (uint8_t i = current_page + 1; i != current_page; i++) {
-		if (mem[i].proc.state == none) {
-			proc = (struct proc*) &mem[i];
-			current_page = i;
-			break;
-		}
-	}
-
-	return proc;
-}
-
-struct process_list_link* get_link () {
-	for (uint8_t i = current_link_slot + 1; i != current_link_slot; i++) {
-		if (! links[i].proc || links[i].proc->state == none) {
-			return &links[i];
-		}
-	}
-	return NULL;
-}
-
-struct process_list_link* find_process(int pid) {
-	for (struct process_list_link* link = head.next; link != NULL; link = link->next) {
-		if (link->proc && link->proc->pid == pid) {
-			return link;
-		}
-	}
-
-	/* Process not loaded, searching link table */
-	printf("/* Process not loaded, searching link table */\n");
-
-	for (uint8_t i = 0; i < 0x100 - 1; i++) {
-		if (links[i].proc->pid == pid)
-			return &links[i];
-	}
-
-	return NULL;
-}
-
-struct proc*
-create_process (int prog_size, int* prog,
-		fifo* input, fifo* output) {
-
-	struct proc* proc = find_page();
-
-	if (proc == NULL) {
-		printf("FATAL: no empty pages\n");
-		exit (1);
-	}
-
-	proc->state = ready;
-
-	proc->input = input;
-	proc->output = output;
-
-	proc->size = prog_size;
-	proc->pc = 0;
-
-	/* copy program into memmory */
-	memcpy (proc_mem(proc), prog, prog_size * sizeof(int));
-	proc->pid = current_pid++;
-
-	struct process_list_link* link = get_link();
-	if (link == NULL) {
-		printf("FATAL: no empty links\n");
-		exit (1);
-	}
-
-	link->next = NULL;
-	link->proc = proc;
-	tail->next = link;
-	tail = link;
-
-	return proc;
-}
-
-int free_process (struct proc* p) {
-	p->state = none;
-	return 0;
-}
 
 void run_asm (proc* p) {
 	instr* in;
@@ -284,16 +141,30 @@ int main () {
 
 						// int any_runing = 1;
 						// while (any_runing) {
-						struct process_list_link* prev = &head;
-						struct process_list_link* link = head.next;
+						struct process_list_link* link = &head;
 						while (head.next != NULL) {
-							if (! link->proc || link->proc->state == none) {
+							link = link->next;
+
+							if (! link) {
+								link = &head;
+								continue;
+							}
+
+							if (! link->proc) {
+								link->prev->next = link->next;
+								link->next->prev = link->prev;
+
+								continue;
+							}
+
+							if (link->proc->state == none) {
 								link->proc = NULL;
-								if (link == tail) {
-									tail = prev;
+								link->prev->next = link->next;
+								if (link->next) {
+									link->next->prev = link->prev;
 								}
-								prev->next = link->next;
-								link = link->next;
+
+								continue;
 							}
 
 							switch (link->proc->state) {
@@ -301,8 +172,8 @@ int main () {
 									printf("Swaping in already swaped in program\n");
 
 								/* fallthrough */
-								case ready:
 								case waiting:
+								case ready:
 #if debug > 0
 									printf("Swaping in %i\n", link->proc->pid);
 #endif
@@ -316,33 +187,19 @@ int main () {
 								/* fallthrough */
 								case done:
 									free_process(link->proc);
+
 									break;
 
 								case none:
 									break;
 							}	
 
-							prev = link;
-							link = link->next;
-							if (link == NULL) {
-								prev = &head;
-								link = head.next;
-							}
 						}	
 
 						int r1;
 						fifo_get(&fifos[4], &r1);
 
-						printf("r1 = %i\n", r1);
-
-#if 0
-						for (struct process_list_link *link = &head;
-								link != NULL;
-								link = link->next) {
-							if (link->proc)
-								free_process(link->proc);
-						}
-#endif
+						// printf("r = %i\n", r1);
 
 						head.next = NULL;
 						tail = &head;
---
+++ 2019/7/process.h
@@ -0,0 +1,150 @@
+#define PAGES (0x100 - 1)
+
+typedef enum process_states {
+	none = 0,
+	ready, running, waiting, done,
+	error,
+} process_state;
+
+char* state_name[] = {
+	[none]    = "none",
+	[ready]   = "ready",
+	[running] = "running",
+	[waiting] = "waiting",
+	[done]    = "done",
+	[error]   = "error",
+};
+
+typedef struct proc {
+	int pid;
+	size_t pc;
+	size_t size;
+	process_state state;
+	fifo *input, *output;
+} proc;
+
+
+/* primary memory */
+struct page { 
+	proc proc;
+	int mem[0x1000];
+} mem[PAGES];
+
+struct process_list_link {
+	struct proc* proc;
+	struct process_list_link* next;
+	struct process_list_link* prev;
+} head, *tail, links[0x100 - 1];
+
+/* Implicit overflow */
+uint8_t current_link_slot = 0;
+
+int current_pid = 0;
+int current_page = 0;
+
+int* proc_mem(proc* p) {
+	return ((int*) (((uint8_t*) p) + sizeof(struct proc)));
+}
+
+void print_process (proc* p) {
+	printf("process = {\n\tpid = %i\n\tpc = %li\n\tstate = %s\n\tmem = [%li]\n\t\t",
+			p->pid, p->pc, state_name[p->state], p->size);
+	for (size_t i = 0; i < p->size; i++) {
+		if (i == p->pc) {
+			printf("[%i], ", proc_mem(p)[i]);
+		} else {
+			printf("%i, ", proc_mem(p)[i]);
+		}
+	}
+	printf("\n\t}\n}\n");
+}
+
+
+struct proc* find_page () {
+	struct proc* proc = NULL;
+
+	// for (uint8_t i = current_page + 1; i != current_page; i++) {
+	for (int i = 0; i < PAGES; i++) {
+		if (mem[i].proc.state == none) {
+			proc = (struct proc*) &mem[i];
+			current_page = i;
+			break;
+		}
+	}
+
+	return proc;
+}
+
+struct process_list_link* get_link () {
+	// for (uint8_t i = current_link_slot + 1; i != current_link_slot; i++) {
+	for (int i = 0; i < 0x100; i++) {
+		if (! links[i].proc || links[i].proc->state == none) {
+			return &links[i];
+		}
+	}
+	return NULL;
+}
+
+struct process_list_link* find_process(int pid) {
+	for (struct process_list_link* link = head.next; link != NULL; link = link->next) {
+		if (link->proc && link->proc->pid == pid) {
+			return link;
+		}
+	}
+
+	/* Process not loaded, searching link table */
+	printf("/* Process not loaded, searching link table */\n");
+
+	for (uint8_t i = 0; i < 0x100 - 1; i++) {
+		if (links[i].proc->pid == pid)
+			return &links[i];
+	}
+
+	return NULL;
+}
+
+struct proc*
+create_process (int prog_size, int* prog,
+		fifo* input, fifo* output) {
+
+	struct proc* proc = find_page();
+
+	if (proc == NULL) {
+		printf("FATAL: no empty pages\n");
+		exit (1);
+	}
+
+	proc->state = ready;
+
+	proc->input = input;
+	proc->output = output;
+
+	proc->size = prog_size;
+	proc->pc = 0;
+
+	/* copy program into memmory */
+	memcpy (proc_mem(proc), prog, prog_size * sizeof(int));
+	proc->pid = current_pid++;
+
+	struct process_list_link* link = get_link();
+	if (link == NULL) {
+		printf("FATAL: no empty links\n");
+		exit (1);
+	}
+
+	link->next = NULL;
+	link->proc = proc;
+	link->prev = tail;
+
+	tail->next = link;
+
+	tail = link;
+
+	return proc;
+}
+
+int free_process (struct proc* p) {
+	p->state = none;
+	return 0;
+}
+
