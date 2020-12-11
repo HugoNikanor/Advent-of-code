a6a52738b213af8f2b1efb8192a7d96350578084
Hugo Hörnquist <hugo@lysator.liu.se>
2020-12-11 10:41:31 +0100

    2020-12-11 ★★

--- 2020/11/main.c
+++ 2020/11/main.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 #define WIDTH  96
-#define HEIGHT  90
+#define HEIGHT 90
 
 char grids[2][HEIGHT][WIDTH];
 
@@ -31,6 +31,28 @@ int main() {
 		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH; x++) {
 				int adj_count = 0;
+
+				for (int i = -1; i <= 1; i++) {
+					for (int j = -1; j <= 1; j++) {
+						if (i == 0 && j == 0) continue;
+						/* [j, i] direction vector */
+						int cx = x, cy = y;
+						do {
+							cx += j;
+							cy += i;
+							if (cx < 0 || cy < 0 || cx >= WIDTH || cy >= HEIGHT) break;;
+							char cell = grids[current][cy][cx];
+							if (cell == '#') {
+								++adj_count;
+								break;
+							} else if (cell == 'L') {
+								break;
+							}
+						} while (1);
+					}
+				}
+
+				/*
 				for (int i = max(0, y - 1); i <= min(HEIGHT - 1, y + 1); i++) {
 					for (int j = max(0, x - 1); j <= min(WIDTH - 1, x + 1); j++) {
 						if (i == y && j == x) continue;
@@ -39,6 +61,7 @@ int main() {
 						}
 					}
 				}
+				*/
 
 				// printf("[%d, %d] = %d\n", x, y, adj_count);
 
@@ -53,7 +76,7 @@ int main() {
 								  break;
 							  }
 					case '#': {
-								  if (adj_count >= 4) {
+								  if (adj_count >= 5) {
 									  grids[other][y][x] = 'L';
 								  } else {
 									  grids[other][y][x] = '#';
@@ -66,7 +89,7 @@ int main() {
 
 		int all_same = 1;
 		int occ_count = 0;
-		printf("\x1b[H\x1b[J");
+		// printf("\x1b[H\x1b[J");
 		printf("loop %d, current = %d, other = %d\n", loops, current, other);
 		for (int y = 0; y < HEIGHT; y++) {
 			for (int x = 0; x < WIDTH; x++) {
