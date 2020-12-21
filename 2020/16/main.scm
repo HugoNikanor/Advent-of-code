6e6c8944929d171823f6aa0d5770d35782e8cd73
Hugo Hörnquist <hugo@lysator.liu.se>
2020-12-22 00:53:38 +0100

    2020-12-16 ★★

--- 2020/16/main.scm
+++ 2020/16/main.scm
@@ -10,16 +10,20 @@
          (cond ((string-match
                  "^([a-z ]*): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)$"
                  line) => (lambda (m)
-                            ;; (display (match:substring m 1)) (newline)
-                            (lambda (x)
-                              (or (<=
-                                   (string->number (match:substring m 2))
-                                   x
-                                   (string->number (match:substring m 3)))
-                                  (<=
-                                   (string->number (match:substring m 4))
-                                   x
-                                   (string->number (match:substring m 5)))))
+                            (cons (string->symbol (string-map
+                                                   (lambda (c)
+                                                     (if (char=? c #\space)
+                                                         #\- c))
+                                                   (match:substring m 1)))
+                                  (lambda (x)
+                                    (or (<=
+                                         (string->number (match:substring m 2))
+                                         x
+                                         (string->number (match:substring m 3)))
+                                        (<=
+                                         (string->number (match:substring m 4))
+                                         x
+                                         (string->number (match:substring m 5))))))
                             ))
                (else (throw 'match-error))))
        (let loop ()
@@ -45,15 +49,95 @@
 
 (define numbers '())
 
-(map (lambda (ticket)
-       ;; (display ticket) (newline)
-       (map (lambda (number)
-              (unless (fold (lambda (a b) (or a b))
-                            #f (map (lambda (p) (p number)) predicates))
-                (set! numbers (cons number numbers))))
-            ticket))
-     tickets)
+(define valid-tickets
+ (filter-map (lambda (ticket)
+               ;; (display ticket) (newline)
+               (and (every (lambda (number)
+                             ;; true if number valides
+                             ;; fales if completely wrong
+                             (fold (lambda (a b) (or a b))
+                                   #f (map (lambda (p) (p number)) (map cdr predicates))))
+                           ticket)
+                    ticket))
+             tickets))
+
+(define categories (array->list (transpose-array (list->array 2 valid-tickets) 1 0)))
+
+;; (display categories)
+;; (newline)
+
+(define valid-tags
+ (map (lambda (category i)
+        (cons i (filter-map
+                 (lambda (pred)
+                   (and (fold (lambda (n valid)
+                                (and valid ((cdr pred) n)))
+                              #t category)
+                        (car pred)))
+                 predicates)))
+      categories
+      (iota (length categories))))
 
 
-(display numbers) (newline)
-(display (apply + numbers)) (newline)
+(define-once pq-magic (gensym))
+(define* (make-priority-queue key compare #:optional (data '()))
+  (list pq-magic key compare data))
+(define (priority-queue? pq) (eq? pq-magic (car pq)))
+(define (priority-queue->list pq)
+  (list-ref pq 3))
+(define (pq-insert item pq)
+  (let ((< (list-ref pq 2))
+        (key (list-ref pq 1)))
+    (make-priority-queue
+     key <
+     (let loop ((pq (list-ref pq 3)))
+       (cond ((null? pq) (cons item '()))
+             ((< (key item) (key (car pq))) (cons item pq))
+             (else (cons (car pq)
+                         (loop (cdr pq)))))))))
+
+
+(define pq
+  (fold (lambda (item q) (pq-insert item q))
+        (make-priority-queue (compose length cdr) <)
+        valid-tags))
+
+(define field-types
+  (let ((set '()))
+   (map (lambda (vtags)
+          (let ((return (lset-difference eq? vtags set)))
+            (set! set (lset-union eq? set vtags))
+            return))
+        (priority-queue->list pq))))
+
+(define indices
+  (filter-map (lambda (field-type)
+                (display field-type) (newline)
+                (if (and (<= (string-length "departure")
+                            (string-length (symbol->string (cadr field-type))))
+                         (string= (symbol->string (cadr field-type))
+                                  "departure" 0 (string-length "departure")))
+                    (car field-type)
+                    #f
+                    ;; (begin (display "\x1b[1m") (display (cadr field-type))
+                    ;;        (display "\x1b[m") (newline))
+                    ;; (begin
+                    ;;   (display (cadr field-type)) (newline))
+                    ))
+              field-types))
+
+(display indices) (newline)
+(display my-ticket)
+(newline)
+(display
+ (apply *
+  (map (lambda (index) (list-ref my-ticket index))
+       indices)))
+
+(newline)
+
+;; (display (length valid-tags))
+;; (newline)
+;; (display (length (priority-queue->list pq)))
+
+;; (newline)
