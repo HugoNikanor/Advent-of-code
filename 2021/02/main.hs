4fbbac68382ef7298000f100d668df4a6cbba9a8
Hugo Hörnquist <hugo@lysator.liu.se>
2021-12-02 11:37:10 +0100

    2021-12-02 ★★

--- 2021/02/main.hs
+++ 2021/02/main.hs
@@ -2,10 +2,10 @@ import Text.Printf
 
 type Instruction = (String, Int)
 
-doInstruction :: Instruction -> (Int, Int) -> (Int, Int)
-doInstruction ("forward", n) (x, y) = (x + n, y)
-doInstruction ("down", n) (x, y) = (x, y + n)
-doInstruction ("up", n) (x, y) = (x, y - n)
+doInstruction :: Instruction -> (Int, Int, Int) -> (Int, Int, Int)
+doInstruction ("forward", n) (x, y, a) = (x + n, y + n * a, a)
+doInstruction ("down", n) (x, y, a) = (x, y, a + n)
+doInstruction ("up", n) (x, y, a) = (x, y, a - n)
 
 parse :: [String] -> Instruction
 parse [s, x] = (s, read x)
@@ -13,5 +13,5 @@ parse [s, x] = (s, read x)
 main = do
     lines' <- lines <$> getContents
     let instructions = parse . words <$> lines'
-    let (x, y) = foldl (flip doInstruction) (0, 0) instructions
+    let (x, y, a) = foldl (flip doInstruction) (0, 0, 0) instructions
     putStrLn $ printf "%d x %d = %d" x y (x * y)
