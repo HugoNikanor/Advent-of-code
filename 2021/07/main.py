fe672abd6efcd226a5a71919bbdd616faa8f6d94
Hugo Hörnquist <hugo@lysator.liu.se>
2021-12-15 17:53:21 +0100

    2021-12-07 ★★

--- 2021/07/main.py
+++ 2021/07/main.py
@@ -2,10 +2,13 @@ ships = [int(x) for x in input().split(',')]
 
 costs = []
 
+def triangel_number(n):
+    return (n * (n + 1)) // 2
+
 for pos in range(max(ships)):
     total_cost = 0
     for ship in ships:
-        total_cost += abs(ship - pos)
+        total_cost += triangel_number(abs(ship - pos))
     costs.append((pos, total_cost))
 
 pos, cost = sorted(costs, key=lambda p: p[1])[0]
