6758065cd1243e5ab1fcb164a73e73eec24c239b
Hugo Hörnquist <hugo@lysator.liu.se>
2021-12-16 00:09:08 +0100

    2021-12-10 ★★

--- 2021/10/main.scm
+++ 2021/10/main.scm
@@ -23,34 +23,56 @@
     ((#\}) 1197)
     ((#\>) 25137)))
 
-(format #t "~a~%"
-        (apply +
-         (map lookup-points
-              (stream->list
-               (stream-map
-                (lambda (line)
-                  (define tokens (string->list line))
-                  (call/ec
-                   (lambda (return)
-                     (fold (lambda (token stack)
-                             (if (memv token '(#\{ #\( #\[ #\<))
-                                 (cons token stack)
-                                 ;; else closing token
-                                 (if (null? stack)
-                                     ;; input incomplete
-                                     (begin
-                                       (format #t "Unexpected end of input~%")
-                                       (return #f))
-                                     (if (char=? (flip token)
-                                                 (car stack))
-                                         ;; correct ending token, pop it off the stack
-                                         (cdr stack)
-                                         ;; incorrect ending
-                                         (begin
-                                           (format #t "Expected `~a`, but found `~a` instead.~%"
-                                                   (flip (car stack)) token)
-                                           (return token))))))
-                           '() tokens)
-                     (return #t)))
-                  )
-                (port->stream (current-input-port) read-line))))))
+(define scores '())
+
+(stream->list
+ (stream-map
+  (lambda (line)
+    (define tokens (string->list line))
+    (format #t "~%~a " line)
+    (call/ec
+     (lambda (return)
+       (define stk
+        (fold (lambda (token stack)
+                (if (memv token '(#\{ #\( #\[ #\<))
+                    (cons token stack)
+                    ;; else closing token
+                    (if (null? stack)
+                        ;; too many closing parenthesee
+                        (return #f)
+                        (if (char=? (flip token)
+                                    (car stack))
+                            ;; correct ending token, pop it off the stack
+                            (cdr stack)
+                            ;; incorrect ending
+                            (begin
+                              (format #t "Expected `~a`, but found `~a` instead."
+                                      (flip (car stack)) token)
+                              (return token))))))
+              '() tokens))
+       (unless (null? stk)
+         (set! scores (cons (fold (lambda (token total)
+                         (+ (* 5 total)
+                            (case token
+                              ((#\)) 1)
+                              ((#\]) 2)
+                              ((#\}) 3)
+                              ((#\>) 4))))
+                                  0 (map flip stk))
+                            scores))
+         (format #t "Complete by adding `~a` ~a points"
+                 (list->string (map flip stk))
+                 (fold (lambda (token total)
+                         (+ (* 5 total)
+                            (case token
+                              ((#\)) 1)
+                              ((#\]) 2)
+                              ((#\}) 3)
+                              ((#\>) 4))))
+                       0 (map flip stk))))
+       (return #t)))
+    )
+  (port->stream (current-input-port) read-line)))
+(newline)
+
+(format #t "middle score = ~a~%" (list-ref (sort scores <) (/ (1- (length scores)) 2)))
