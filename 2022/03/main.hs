881fff364e0921cd910ffce4ac184516575dbdde
Hugo Hörnquist <hugo@lysator.liu.se>
2022-12-25 21:24:53 +0100

    2022-12-03 ★★

--- 2022/03/main.hs
+++ 2022/03/main.hs
@@ -8,10 +8,31 @@ item2Priority i
     | 'a' <= i && i <= 'z' = ord i - ord 'a' + 1
     | 'A' <= i && i <= 'Z' = ord i - ord 'A' + 1 + 26
 
-handleLine line = Set.intersection (Set.fromList left) (Set.fromList right)
+(&) = flip ($)
+
+-- part 1
+handleLine1 line = Set.intersection (Set.fromList left) (Set.fromList right)
     where (left, right) = splitAt (length line `div` 2) line
+
+-- part 2
+findIntersecting :: Ord a => Set a -> [Set a] -> (Set a, [Set a])
+findIntersecting y (x:xs) = if not $ null inter
+        then (inter, xs)
+        else (x:) <$> findIntersecting y xs
+    where inter = Set.intersection x y
+
+findGroup :: Ord a => [Set a] -> (Set a, [Set a])
+findGroup (x:xs) = findIntersecting x xs & uncurry findIntersecting
+
+findGroups :: Ord a => [Set a] -> [Set a]
+findGroups [] = []
+findGroups xs = group : findGroups rest
+                where (group, rest) = findGroup xs
 
 main :: IO ()
 main = do
     rucksacks <- lines <$> getContents
-    print $ sum $ item2Priority . Set.elemAt 0 . handleLine <$> rucksacks
+    -- part 1
+    -- print $ sum $ item2Priority . Set.elemAt 0 . handleLine1 <$> rucksacks
+    -- part 2
+    print . sum . fmap (item2Priority . Set.elemAt 0) . findGroups $ Set.fromList <$> rucksacks
