2c81f2eefa780e56d4fbe9d7f5400d12506da0ef
Hugo Hörnquist <hugo@lysator.liu.se>
2022-12-25 12:29:26 +0100

    2022-12-02 ★★

--- 2022/02/main.hs
+++ 2022/02/main.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables  #-}
 
+import Control.Lens
+
 data RPS = Rock | Paper | Scissors deriving (Show, Eq)
 
 instance Enum RPS where
@@ -9,29 +11,41 @@ instance Enum RPS where
     fromEnum Rock     = 1
     fromEnum Paper    = 2
     fromEnum Scissors = 3
+    succ Rock     = Paper
+    succ Paper    = Scissors
+    succ Scissors = Rock
+    pred Paper     = Rock    
+    pred Scissors  = Paper   
+    pred Rock      = Scissors
 
 parse "A" = Rock
-parse "X" = Rock
 parse "B" = Paper
-parse "Y" = Paper
 parse "C" = Scissors
-parse "Z" = Scissors
 
 instance Ord RPS where
     Rock     < Paper    = True
     Paper    < Scissors = True
     Scissors < Rock     = True
     a        < b        = False
+    -- `a < b = a < succ b` doesn't work since 
+    -- `fromEnum (succ b) < fromEnum a` may be true
     a <= b = a < b || a == b
 
+data Result = Win | Draw | Loss deriving (Show, Eq)
 
-calculateRound :: [RPS] -> Int
-calculateRound [a, x]
-    | a < x     = 6 + fromEnum x
-    | a == x    = 3 + fromEnum x
-    | otherwise = 0 + fromEnum x
+parseR "X" = Loss
+parseR "Y" = Draw
+parseR "Z" = Win
+
+calculateRound :: RPS -> Result -> Int
+calculateRound x Win  = 6 + fromEnum (succ x)
+calculateRound x Draw = 3 + fromEnum x
+calculateRound x Loss = fromEnum (pred x)
+
+toTup2 :: [a] -> (a, a)
+toTup2 [a, b] = (a, b)
 
 main :: IO ()
 main = do
-    input :: [[RPS]] <- fmap (fmap parse . words) . lines <$> getContents
-    print . sum $ calculateRound <$> input
+    input :: [(RPS, Result)] <- fmap ((_2 %~ parseR) . (_1 %~ parse) . toTup2 . words) . lines <$> getContents
+    print . sum $ uncurry calculateRound <$> input
