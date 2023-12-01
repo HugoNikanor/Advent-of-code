f53d0046581e2c94401fdeea8874a967fa337ea4
Hugo Hörnquist <hugo@lysator.liu.se>
2023-12-01 21:44:23 +0100

    2023-12-01 ★★

--- 2023/01/main.py
+++ 2023/01/main.py
@@ -3,14 +3,32 @@
 import sys
 import re
 
+nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+
+
+def parseint(x: str, rev=False) -> int:
+    try:
+        return int(x)
+    except ValueError:
+        if rev:
+            lst = [''.join(reversed(s)) for s in nums]
+        else:
+            lst = nums
+        return lst.index(x) + 1
+
+
 sum = 0
 for line in sys.stdin:
     if not line:
         continue
-    m = re.findall(r'\d', line)
-    t = int(m[0] + m[-1])
+    rx = r'one|two|three|four|five|six|seven|eight|nine'
+    m1 = re.findall(r'\d|' + rx, line)
+    m2 = re.findall(r'\d|' + ''.join(reversed(rx)),
+                    ''.join(reversed(line)))
+    t = int(str(parseint(m1[0])) + str(parseint(m2[0], rev=True)))
     sum += t
-    print(t)
+    print(t, " ", line.strip())
 
 print()
 print(sum)
